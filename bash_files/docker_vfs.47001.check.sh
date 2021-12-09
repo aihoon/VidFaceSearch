7d3ec7d316b30e52e92282d62d1f282cd3d1bc86
HOON <hoon.paek@mindslab.ai>
2021-12-08 21:48:36 -0800

    update

---
+++ bash_files/docker_vfs.47001.check.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+curl -X POST -H "Content-Type: application/json"  http://0.0.0.0:47001/check
---
+++ bash_files/docker_vfs_47001_run.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+corename = "20211119_cc1"
+base_folder = "/mnt/SHARED/VFS"
+k
+
+
+
+
+
+
+
+
+curl -X POST http://0.0.0.0:47001/run \
+  -H "Content-Type: application/json" \
+  -d '{"vid_content_meta_url": "/mnt/SHARED/./Input/20211119_cc1/20211119_cc1.vcm.json",
+       "src_vid_url":"./Input/20211119_cc1//20211119_cc1.mp4",
+       "infected_picture_url":"./Input/20211119_cc1/infected_face1.png",
+       "rst_vid_url":"./Client/20211119_cc1.vfs.mp4",
+       "rst_img_folder_url":"./Client/Details",
+       "N": "100"}'
+#   -d '{"vid_content_meta_url": "./Input/S#_5_6_7_8/S#_5_6_7_8.vcm.json",
+#        "src_vid_url":"./Input/S#_5_6_7_8/S#_5_6_7_8.mp4",
+#        "infected_picture_url":"./Input/S#_5_6_7_8/infected_face.png",
+#        "rst_vid_url":"./Client/S#_5_6_7_8.vfs.mp4",
+#        "rst_img_folder_url":"./Client/Details",
+#        "N": "100"}'
