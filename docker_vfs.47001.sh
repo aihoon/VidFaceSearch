233868d87b28e280ad544a134b01e0e0a8d9c6db
HOON <hoon.paek@mindslab.ai>
2021-12-08 20:36:55 -0800

    update

--- docker_vfs.47001.sh
+++ docker_vfs.47001.sh
@@ -1,2 +1,4 @@
-python ./VidFaceSearch.py --ini_fname docker_vfs.47000.ini
+nohup python ./VidFaceSearch.py --ini_fname  ./ini_files/docker_vfs.47001.ini >& docker_vfs.47001.nohup.out &
+tail -f docker_vfs.47001.nohup.out
+
 
